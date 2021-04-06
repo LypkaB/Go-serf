49ec0adec4a126bc04768fe442877bd7685d12a2
LypkaB <lpk.bdn@gmail.com>
2021-04-06 19:31:49 +0300

    Added section «Presentation»

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    /*<----- Presentation slider ----->*/
+    const presentationSlider = tns({
+        container: '.presentation__content-slider',
+        navContainer: '.presentation__content-pagination',
+        controlsContainer: '.presentation__content-controls'
+    })
+})
