25df9270de10b8a6f97aac7bb3c21ce2b9584251
LypkaB <lpk.bdn@gmail.com>
2021-04-08 22:57:21 +0300

    Added adaptive for section «Presentation»

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,24 @@ window.addEventListener('DOMContentLoaded', () => {
         navContainer: '.presentation__content-pagination',
         controlsContainer: '.presentation__content-controls'
     })
+
+    /*<----- Presentation sidebar ----->*/
+    const arrowsPresentation = document.querySelector('.presentation__arrows');
+    let sidebarBtnClicks = 0;
+
+    document.querySelector('.presentation__sidebar-btn').addEventListener('click', () => {
+        document.querySelector('.presentation__sidebar').classList.toggle('sidebar_active');
+        arrowsPresentation.classList.toggle('arrows_hidden');
+        sidebarBtnClicks++;
+        arrowsPresentation.classList.toggle('arrows_show-anim',sidebarBtnClicks % 2 === 0);
+    })
+
+    /*<----- Adaptive for slider map ----->*/
+    const mapPresentation = document.querySelectorAll('.presentation__content-map svg');
+
+    if (document.querySelector('body').clientHeight >= 940) {
+        mapPresentation.forEach( (item, i) => {
+            mapPresentation[i].setAttribute('height', '700');
+        })
+    }
 })
